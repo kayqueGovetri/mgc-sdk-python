42998989a7f31e0c80c77830217100204c4da5a3
kayqueGovetri <govetrikayque@gmail.com>
2026-08-12 18:36:43 -0300

    style: fix import unsorted

--- examples/object_storage/buckets/create.py
+++ examples/object_storage/buckets/create.py
@@ -1,6 +1,5 @@
 from mgc.resources.object_storage.object_storage import ObjectStorage
 
-
 storage = ObjectStorage(
     access_key="YOUR_ACCESS_KEY",
     secret_key="YOUR_SECRET_KEY",
--- examples/object_storage/buckets/set_policy.py
+++ examples/object_storage/buckets/set_policy.py
@@ -2,7 +2,6 @@ import json
 
 from mgc.resources.object_storage.object_storage import ObjectStorage
 
-
 storage = ObjectStorage(
     access_key="YOUR_ACCESS_KEY",
     secret_key="YOUR_SECRET_KEY",
